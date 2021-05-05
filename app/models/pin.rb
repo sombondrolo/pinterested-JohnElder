c4ef083cfa9d91c93a456199dd79c63710b532dc
Fer Candia <sombondrolo@gmail.com>
2021-05-05 00:27:19 -0300

    added attachment image to pins, modified pin db table

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,5 +1,7 @@
 class Pin < ApplicationRecord
   belongs_to :user
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  has_attached_file :image,
+    :styles => { :medium => "300x300>", :thumb => "100x100>" }
   # validates_attachment_content_type :image, :content_type => /\Aimage\/.*\z/ #\Aimage\.*\Z/
+  validates :image, attachment_content_type: { content_type: /\Aimage\/.*\z/ }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_04_194800) do
+ActiveRecord::Schema.define(version: 2021_05_05_024704) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,10 @@ ActiveRecord::Schema.define(version: 2021_05_04_194800) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.bigint "image_file_size"
+    t.datetime "image_updated_at"
     t.index ["user_id"], name: "index_pins_on_user_id"
   end
 
